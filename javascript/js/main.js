e0ade6dbeb76879529b9529a434bb7ae55ea537b
barbaraarruda <barbarahmiley@gmail.com>
2022-10-07 10:15:30 -0300

    aula de array/dict

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,12 +1,36 @@
-var nome = "Bárbara Arruda";
-var idade = 24;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo";
+
+
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã", "pêra", "laranja"];
+///lista.push("uva"); //acrescenta elementos na lista
+//lista.pop(); //retira elemento. Aqui ele retirou o último.
+//console.log(lista[0]);
+//alert(lista[1]);
+///console.log(lista.length); //tamannho da lista.
+//console.log(lista.reverse()); //esse é um método. Inverte a ordem dos elmentos da lista.
+//console.log(lista.toString()); //tira a array e vira string. Quando especifica apenas um elemento [], imprime apenas a letra referente.
+//console.log(lista.join(" - ")); //coloca o que quiser entre os elementos.
+
+
+
+
+//var nome = "Bárbara Arruda";
+//var idade = 24;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade + " anos"); //adiciona pop-up na página.
 //alert(idade + idade2)
-
-console.log(nome);
-console.log(idade);
+//console.log(nome);
+//console.log(idade);
 ///console.log(frase.replace("Japão", "Coreia do Sul"));
-console.log(frase.toUpperCase());
+//console.log(frase.toUpperCase());
 ///alert(frase.replace("Japão", "Coreia do Sul");
