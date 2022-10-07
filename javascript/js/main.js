0d7aee36232280d192e70f9997600d1cf1cd6417
barbaraarruda <barbarahmiley@gmail.com>
2022-10-07 09:54:52 -0300

    primeira aula de prog js

---
+++ javascript/js/main.js
@@ -0,0 +1,12 @@
+var nome = "Bárbara Arruda";
+var idade = 24;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos"); //adiciona pop-up na página.
+//alert(idade + idade2)
+
+console.log(nome);
+console.log(idade);
+///console.log(frase.replace("Japão", "Coreia do Sul"));
+console.log(frase.toUpperCase());
+///alert(frase.replace("Japão", "Coreia do Sul");
