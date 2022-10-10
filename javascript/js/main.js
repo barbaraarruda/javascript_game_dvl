4300fa8197c56506b5ba9177b830d016d9219ef5
barbaraarruda <barbarahmiley@gmail.com>
2022-10-10 16:44:34 -0300

    encerrou aula de prog para internet com javascript

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,7 +1,42 @@
+function clicou(){
 
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar.</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://www.google.com/"); //abre em nova aba.
+    //window.location.href = "https://www.google.com/"; //esse redireciona automaticamente.
+
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui" //esse método é alternativo ao document.getelementbyid
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+/*
 function soma(n1, n2){
     return n1 + n2;
 }
+*/
+
+/*
 
 function validaIdade(idade){
     var validar; //variável local
@@ -15,6 +50,7 @@ function validaIdade(idade){
 
 var idade = prompt("Qual sua idade?");
 console.log(validaIdade(idade));
+*/
 
 
 //alert(soma(5,10));
